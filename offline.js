231898bbe07b541d231304025c7d14331923173d
senukisandamini <83801985+senukisandamini@users.noreply.github.com>
2021-08-21 22:41:57 -0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1629655776,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/photo1569982175971d92b01cf-sheet0.png",
+		"images/pngclipartsoapbubblepurpleandyellowbubblessoapbubblebubblethumbnailremovebgpreview-sheet0.png",
+		"images/particles.png",
+		"images/particles2.png",
+		"images/sprite-sheet0.png",
+		"images/63106475childrencampingontheislandillustration-sheet0.png",
+		"images/beautifulcartoonseafishanimestyleisolatedwhitebackgroundvideogamesdigitalcgartworkconceptill-sheet0.png",
+		"images/8a466b6e59c6a0da495b5fff9b5b3cee-sheet0.png",
+		"images/tiledbackground.png",
+		"images/pngtreecutecartoongameoverpngimage_6022480removebgpreview-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/restart-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
